d3bcb1e7e601cb5ec6d41de4baa3fd3a9e22a9b1
asullivan2025 <a.sullivan.2025@gmail.com>
2022-01-08 18:14:14 +0000

    paging for /recipes

--- app.js
+++ app.js
@@ -10,55 +10,23 @@ initializeSequelize().then((sequelize) => {
 
   const { measurementUnit, ingredient, recipe, recipeIngredient, user, recipeStep } = sequelize.models;
 
-  app.get("/", (req, res) => res.send("Hello World!"));
-  app.get("/products", (req, res) => {
-    const products = [
-      {
-        id: 1,
-        name: "hammer",
-      },
-      {
-        id: 2,
-        name: "screwdriver",
-      },
-      ,
-      {
-        id: 3,
-        name: "wrench",
-      },
-    ];
-
-    res.json(products);
-  })
-
-  app.post("/dad", (req, res) => {
-    res.send("Hello world");
-  })
-
-  app.get("/dad", (req, res) => {
-    res.send("Hello world dad");
-  })
-
-  app.get("/users/:userID", (req, res) => {
-    res.send("user id :" + req.params.userID)
-  })
-
-  app.post("/monkey", (req, res) => {
-    console.log(req.body);
-    res.send("okay");
-  })
-
-
   app.get("/recipes", async (req, res) => {
+    const {sort, limit, offset}=req.query;
+    const limitNum = parseInt(limit);
+    const offsetNum = parseInt(offset);
+    const actualLimit = isNaN(limitNum)?50:Math.min(100, limitNum);
+    const actualOffset = isNaN(offsetNum)?0:offsetNum;
+    
     const recipes = await recipe.findAll(
       {
-        order: [['createdAt', 'ASC'], [recipeStep, "order", "ASC"]],
+        order: [['createdAt', req.query.sort === "DESC" ? 'DESC' : 'ASC'], [recipeStep, "order", "ASC"]],
         include: [{
           model: recipeIngredient, include: [ingredient, measurementUnit]
         },
         {
           model: recipeStep
-        }]
+        }],
+        limit: actualLimit, offset: actualOffset
       }
     );
     res.json(recipes);
